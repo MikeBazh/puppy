f5309a2fda6c77cfa9819f8c63f407bfc14fe602
My Name <myEmail@example.com>
2023-06-27 21:31:03 +0800

    BigBark added

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/MikeBazh/puppy
 
 go 1.20
+
+require github.com/MikeBazh/dog v0.0.0-20230627131320-fb5d5fd42b3b // indirect
--- puppy.go
+++ puppy.go
@@ -1,5 +1,12 @@
 package puppy
 
+import (
+	"github.com/MikeBazh/dog"
+)
+
 func Bark() string {
 	return "Woof!!!!"
+}
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
 }
